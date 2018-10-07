db764e741c2b526f7c19bdbb6cf3465aa938ebf0
Ajit Gevariya <gevariyaajit@gmail.com>
2018-10-07 11:41:50 +0530

    Plant timber tree

--- app/src/main/kotlin/com/github/repo/GitApp.kt
+++ app/src/main/kotlin/com/github/repo/GitApp.kt
@@ -3,12 +3,14 @@ package com.github.repo
 import android.app.Application
 import com.github.repo.di.AppModule
 import org.koin.android.ext.android.startKoin
+import timber.log.Timber
 
 class GitApp: Application(){
-    
+
     override fun onCreate() {
         super.onCreate()
 
         startKoin(this, listOf(AppModule.getModule()))
+        Timber.plant(Timber.DebugTree())
     }
 }
